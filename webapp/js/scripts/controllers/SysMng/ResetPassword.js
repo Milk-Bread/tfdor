26818c9f4223331b1493d747da4becd68c24361e
chepeiqing <chepeiqing@csii.com.cn>
2016-11-11 18:38:28 +0800

    commit

---
+++ webapp/js/scripts/controllers/SysMng/ResetPassword.js
@@ -0,0 +1,28 @@
+define(['app', 'service', 'sysCode'], function (app) {
+    app.controller('restPasdCtrl', function (service, $scope, $location, $state, $stateParams) {
+        $scope.doIt = function() {
+            var password = $scope.password;
+            if (password == undefined || password == '') {
+                showError("错误提示：请输入新密码");
+                return;
+            };
+            var confirmPassword = $scope.confirmPassword;
+            if (confirmPassword == undefined || confirmPassword == '') {
+                showError("错误提示：请输入确认密码");
+                return;
+            };
+            if(password != confirmPassword){
+                showError("错误提示：两次密码不一致");
+                return;
+            };
+            var formData = {
+                "password": password,
+                "confirmPassword": confirmPassword
+            };
+            service.post2SRV("resetPasd.do", formData, function (data, status) {
+                service.setUser(data);
+                $state.go("Main");
+            }, 4000);
+        }
+    });
+});
