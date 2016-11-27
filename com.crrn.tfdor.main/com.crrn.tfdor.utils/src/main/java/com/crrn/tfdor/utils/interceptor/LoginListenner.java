1b303c5f3d0c4bff3c7243b43ddb47cc221987ac
chepeiqing <chepeiqing@csii.com.cn>
2016-11-27 12:22:31 +0800

    门户网站模版

---
+++ com.crrn.tfdor.main/com.crrn.tfdor.utils/src/main/java/com/crrn/tfdor/utils/interceptor/LoginListenner.java
@@ -0,0 +1,18 @@
+package com.crrn.tfdor.utils.interceptor;/**
+ * @author chepeiqing
+ * @Mail chepeiqin@icloud.com
+ * @Date 2016/11/24
+ * @Time 上午12:04
+ * @version V1.0.0
+ */
+
+/**
+ *
+ *@author chepeiqing
+ *@Mail chepeiqin@icloud.com
+ *@Date 2016/11/24
+ *@Time 上午12:04
+ *@version V1.0.0
+ */
+public class LoginListenner {
+}
---
+++ web/js/service/service.js
@@ -0,0 +1,41 @@
+define(['app'], function (app) {
+    app.service('service', function ($http, $state) {
+        this.post2SRV = function (action, formData, callBack, timeOut) {
+            if (formData == null || formData == '') {
+                formData = {};
+            }
+            formData["transName"] = action;
+            transFn = function (formData) {
+                return angular.param(formData);
+            }
+            postCfg = {
+                headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+                transformRequest: transFn
+            };
+            $http.post(
+                action,
+                formData,
+                postCfg,
+                timeOut
+            ).success(function (data, header, config, status) {
+                if (data._exceptionCode != null || data._exceptionCode == 'false') {
+                    showError("错误提示：" + data._exceptionMsg);
+                } else {
+                    callBack(data, header, config, status);
+                }
+            }).error(function (data, header, config, status) {
+                var errorStr = "网络错误";
+                if (data._exceptionMsg != null && data._exceptionMsg != undefined && data._exceptionMsg != '') {
+                    errorStr = data._exceptionMsg;
+                }
+                showError("错误提示：" + errorStr + action);
+            });
+        };
+        this.getData = function () {
+            return angular.fromJson(sessionStorage.getItem("paramData"));
+        };
+        this.setData = function (data) {
+            sessionStorage.setItem("paramData", angular.toJson(data));
+        };
+    });
+});
