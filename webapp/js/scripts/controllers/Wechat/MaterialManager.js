f08229eb12644c7dd303a6dd69a0460c48a72d36
chepeiqing <chepeiqing@csii.com.cn>
2016-12-05 13:10:52 +0800

    新增用户ID查询

--- webapp/js/scripts/controllers/Wechat/MaterialManager.js
+++ webapp/js/scripts/controllers/Wechat/MaterialManager.js
@@ -1,30 +1,64 @@
-define(['app', 'service', 'sysCode','kindeditorAll','kindeditorditor'], function (app) {
+define(['app', 'service', 'sysCode', 'kindeditorAll', 'kindeditorditor'], function (app) {
     app.controller('materialCtrl', function (service, $scope, $state) {
         $scope.indexList;
         $scope.init = function () {
             $scope.materList = [{
-             "img":"","title":"aaaassss"
-            },{
-                "img":"","title":"bbb"
+                "img": "", "title": "这里是标题"
+            }, {
+                "img": "", "title": "这个也是标题"
             }];
         }
-        $scope.addMaterial = function(){
-            $scope.materList.push({"img":"","title":""});
+        $scope.addMaterial = function () {
+            $scope.materList.push({"img": "", "title": ""});
         };
-        $scope.deleteMaterial = function(index){
-            $scope.materList.splice(index,1);
+        $scope.deleteMaterial = function (index) {
+            if ($("#div" + index).attr("class").indexOf("materialDiv") >= 0) {
+                if($("input[name=titleContext]").val() != ""){
+                    if(!confirm("是否放弃修改")){
+                        return;
+                    }else{
+                        $("input[name=titleContext]").val("");
+                        $scope.materList.splice(index, 1);
+                    }
+                }else{
+                    $("input[name=titleContext]").val("");
+                    $scope.materList.splice(index, 1);
+                }
+            }
         };
-        $scope.clickMaterial = function(index){
-            $scope.indexList = 'title'+index;
-            if($("#div"+index).attr("class").indexOf("materialDiv")>=0){
+
+        $scope.clickMaterial = function (index) {
+            if ($("#div" + index).attr("class").indexOf("materialDiv") >= 0) {
                 return;
             }
-            $("div[name=materialDiv]").attr("class","material_imgdiv");
-            $("#div"+index).addClass("materialDiv");
+            if($("input[name=titleContext]").val() != ""){
+                if(!confirm("是否放弃修改")){
+                    return;
+                }else{
+                    $("#" + $scope.oldTital.id).text($scope.oldTital.content);
+                }
+            }
+            $scope.oldTital = {"id":"title"+index,"content":$('#title' + index).text()};
+            $scope.indexList = 'title' + index;
+            $("div[name=materialDiv]").attr("class", "material_imgdiv");
+            $("#div" + index).addClass("materialDiv");
             $("input[name=titleContext]").val("");
         };
-        $scope.$watch('titleContext',function(scope){
-            $("#"+$scope.indexList).text($scope.titleContext);
+        $scope.$watch('titleContext', function (scope) {
+            if ($scope.count == undefined) {
+                $scope.count = 2;
+                return;
+            } else if ($scope.count > 1) {
+                if($scope.indexList == undefined){
+                    showError("请选择图文信息");
+                    $("input[name=titleContext]").val("");
+                    return;
+                }
+                $("#" + $scope.indexList).text($scope.titleContext);
+                if($("input[name=titleContext]").val() == ""){
+                    $("#" + $scope.oldTital.id).text($scope.oldTital.content);
+                }
+            }
         });
         $scope.init();
     });
